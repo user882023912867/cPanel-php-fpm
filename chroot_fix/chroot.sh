cca70345a2b1168e78ddb2fdc8743e901dc9a8c9
magenx <designelo@gmail.com>
2015-10-13 19:59:18 +0100

    Create chroot.sh

---
+++ chroot_fix/chroot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir {dev,pki,etc}
+cp /etc/nsswitch.conf  etc/
+cp /etc/resolv.conf  etc/
+cp -rf /etc/pki/{CA,ca-trust,tls,nssdb} pki/
+cp /lib64/{libsoftokn3.*,libnss*,libfreebl*} lib64/
+mknod -m 666 dev/urandom c 1 9
