b30e98dd05bf9dd23abd56ac2c3d914c31f450b4
magenx <designelo@gmail.com>
2015-10-13 20:27:36 +0100

    Delete chroot.sh

--- chroot_fix/chroot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir {dev,pki,etc,lib64}
-cp /etc/nsswitch.conf  etc/
-cp /etc/resolv.conf  etc/
-cp -rf /etc/pki/{CA,ca-trust,tls,nssdb} pki/
-cp /lib64/{libsoftokn3.*,libnss*,libfreebl*} lib64/
-mknod -m 666 dev/urandom c 1 9
